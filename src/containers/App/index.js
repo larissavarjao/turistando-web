d62f093cf2f80336e0f2de9be244ffc31460e2dc
Larissa <larissasilvavarjao@gmail.com>
2020-04-16 23:59:26 -0300

    feat(auth): auth structure

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../Home';
+import Auth from '../Auth';
 
 function App() {
-  return (
-    <div className='App'>
-      <Home />
-    </div>
-  );
+  const isLogged = false;
+
+  if (!isLogged) {
+    return (
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/auth' component={Auth} />
+        <Redirect path='*' to={Home} />
+      </Switch>
+    );
+  }
+
+  return <div>Dashboard</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App/index';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+
 import { theme, GlobalStyle } from './style/globalStyle';
+import App from './containers/App/index';
+import { history } from './context/history';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter history={history}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
