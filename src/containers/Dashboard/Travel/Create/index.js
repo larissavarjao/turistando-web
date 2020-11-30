a458bf6162d4b525ea7c47802ce8b1b9b4535e07
Larissa Varjão <larissasilvavarjao@gmail.com>
2020-11-30 17:29:24 -0300

    feat: add lint and prettier

--- src/containers/Dashboard/Travel/Create/index.js
+++ src/containers/Dashboard/Travel/Create/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { PageHeader } from '../../../../components/Header';
 import { Title } from '../../../../components/Typography';
+import { Input, InputContainer, Label } from '../../../../components/Input';
+import { ErrorFormMessage } from '../../../../components/Message';
 
 function TravelCreate() {
+  const [name, setName] = useState('');
+  const [nameError, setNameError] = useState('');
+
   return (
     <div>
       <PageHeader>
         <Title>Criar Viagem</Title>
       </PageHeader>
+      <div>
+        <InputContainer width="100%">
+          <Input
+            type="text"
+            id="email"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <Label htmlFor="email">E-mail</Label>
+          {nameError && <ErrorFormMessage message={nameError} />}
+        </InputContainer>
+      </div>
     </div>
   );
 }
